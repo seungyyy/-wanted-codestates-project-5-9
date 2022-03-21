ecf658f2361dd9fbe96e40395979c8d93a65713d
seungyeon.lee <dus3214@gmail.com>
2022-03-21 13:43:50 +0900

    fix: 초기세팅

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
 
+const axios = require('axios');
+const cheerio = require('cheerio');
+const log = console.log;
+
+
 function App() {
+  
+  const getHtml = async () => {
+    try {
+      return await axios.get(
+        'https://balaan.co.kr/shop/pc2m.php?landing_url=/m2/main/contents.php'
+      ); // axios.get 함수를 이용하여 비동기로 네이버의 html 파일을 가져온다.
+    } catch (error) { console.error(error); }
+  };
+  getHtml()
+    .then(html => {
+      let ulList = [];
+      const $ = cheerio.load(html.data);
+      const $bodyList = $('#contents-list').children('#contents-list > div:nth-child(1)');
+      $bodyList.each(function (i, elem) {
+        ulList.push({
+          url: $(this).find('img.contents-for-grid').attr('src'),
+          nick: $(this).find('a.contents-editor').text(),
+        });
+      });
+      return ulList;
+    }).then(res => log(res));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }

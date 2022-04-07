a215810f790f446873a79550576b91c93c18b6da
seungyeon.lee <dus3214@gmail.com>
2022-04-07 22:44:16 +0900

    feat: 리덕스 배포용 개발용 세팅

--- src/state/store.js
+++ src/state/store.js
@@ -1,5 +1,10 @@
-import { createStore } from "redux";
+import { createStore, compose, applyMiddleware } from 'redux';
 import rootReducer from "./reducers";
 import { composeWithDevTools } from 'redux-devtools-extension';
+import logger from 'redux-logger';
 
-export const store = createStore(rootReducer, composeWithDevTools());
+export const store = createStore(
+  rootReducer,
+  process.env.NODE_ENV === 'production' ? compose() : composeWithDevTools(applyMiddleware(logger))
+);
+ 
